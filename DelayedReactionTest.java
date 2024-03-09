02f8183ce9b3cd755ad0ef6b3269bcc9d02f65ed
Anastasia <156694058+Anastasia-Klestova@users.noreply.github.com>
2024-03-09 21:47:32 +0700

    Add files via upload

---
+++ DelayedReactionTest.java
@@ -0,0 +1,70 @@
+package ru.academits.reqres;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.Matchers.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DelayedReactionTest {
+
+    @Test
+    public void delayedReactionParamTest() {
+        ValidatableResponse response = RestAssured
+                .given()
+                .when()
+                .get("https://reqres.in/api/data ")
+                .then();
+        System.out.println("Response: " + response.extract().asPrettyString());
+        System.out.println("Report-To: " + response.extract().header("Report-To"));
+    }
+
+    @Test
+    public void statusCodeTest() {
+        Response response = RestAssured
+                .get("https://reqres.in/api/users?delay=3")
+                .andReturn();
+        response.prettyPrint();
+        int statusCode = response.getStatusCode();
+        System.out.println("Status code: " + statusCode);
+        assertEquals(200, response.statusCode(), "Unexpected status code");
+
+    }
+
+    @Test
+    public void testResponseTime() {
+        ValidatableResponse response = RestAssured
+                .given()
+                .when()
+                .get("https://reqres.in/api/?delay")
+                .then()
+                .assertThat()
+                .time(lessThanOrEqualTo(7000L));
+
+    }
+
+    @Test
+    public void testResponseBody() {
+        ValidatableResponse response = RestAssured
+                .given()
+                .when()
+                .get("https://reqres.in/api/users?delay")
+                .then()
+                .body("data.id", notNullValue())
+                .body("data.email", notNullValue())
+                .body("data.first_name", notNullValue())
+                .body("data.last_name", notNullValue());
+    }
+
+    @Test
+    public void testInvalidRequest() {
+        ValidatableResponse response = RestAssured
+                .given()
+                .when()
+                .get("https://reqres.in/api/?delay=3")
+                .then()
+                .statusCode(404);
+    }
+}
